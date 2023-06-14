77db7a4cce5d8bb6354996fe8108ed08ef9a401c
Diego Go <sajudian85@gmail.com>
2023-06-13 22:47:54 -0500

    Mas cambios

--- jsLaInvita.js
+++ jsLaInvita.js
@@ -1,14 +1,12 @@
 const typed = new Typed('.typed', {
    strings: ['Un día yo estaba como tú; viendo algo que llamó mi atención en la universidad. Después de eso pasaron cosas tan extrañas que llegué a sentir todo como un sueño surrealista. <br> Cosas que me hicieron dudar de mi forma de actuar. Mi forma de pensar. Cosas que me hicieron hacer otras cosas, de las cuales no sé si soy'],
-   typeSpeed: 35
+   typeSpeed: 35,
+   cursorChar: ''
 });
-
-//const mclarens = new Typed('.boton')
-var parrafo2; //PENDIENTEEEEEEEEE
 
 setTimeout(() => {
    var boton = document.querySelectorAll('.botones .boton');
-
+   
    for (let i = 0; i < boton.length; i++) {
       boton[i].style.backgroundColor = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5)`
    }
@@ -16,6 +14,7 @@ setTimeout(() => {
 
 setTimeout(() => {
    var boton = document.querySelectorAll('.botones .boton');
+   /*boton.forEach(element => (element).style.display = "flex");*/
    var opciones = ["Culpable", "Complice", "Elegido", "Observador"];
    for (let i = 0; i < boton.length; i++) {
       boton[i].innerHTML = `${opciones[i]}`;
@@ -31,6 +30,54 @@ culpable.addEventListener('click', siguienteParrafo);
 complice.addEventListener('click', siguienteParrafo);
 elegido.addEventListener('click', siguienteParrafo);
 observador.addEventListener('click', siguienteParrafo);
+
+var parrafo2; //PENDIENTEEEEEEEEE
+var parrafo3; //PARA NEW TYPED
+var venganza = document.getElementById('parrafo2'), interes = document.getElementById('parrafo2'), verdad = document.getElementById('parrafo2');
+
+function mostrarParrafo3(e) {
+   console.log("funciona");
+   let palabra;
+   switch (e.target.id) {
+      case "venganza":
+         palabra = "hechos";
+         break;
+      case "interes":
+         palabra = "********";
+         break;
+      case "verdad":
+         palabra = "crímenes";
+         break;
+      default:
+         break;
+   }
+
+   var texto = `Los sueños pueden ser una fachada precisamente de los detalles de aquel plan de tantos ${palabra}.`;
+   var parrafo = document.getElementById('parrafo3');
+   var pizarra = document.createElement("div");
+   pizarra.classList.add("texto");
+   pizarra.classList.add("texto3");
+
+   parrafo.appendChild(pizarra);
+
+   setTimeout(() => {
+      var botones = document.createElement("div");
+      botones.setAttribute("class", "botones");
+      parrafo.appendChild(botones);
+
+      botones.innerHTML = '<button id="venganza" class="boton">¿Por qué soñamos?</button>';
+   }, 5000)
+
+   parrafo3 = new Typed('.texto3', {
+      strings: [texto],
+      typeSpeed: 35
+   })
+
+}
+
+venganza.addEventListener('click', mostrarParrafo3);
+interes.addEventListener('click', mostrarParrafo3);
+verdad.addEventListener('click', mostrarParrafo3);
 
 function siguienteParrafo(e) {
    var palabra;
@@ -84,7 +131,7 @@ function siguienteParrafo(e) {
       botones.innerHTML = '<button id="venganza" class="boton">¿Por venganza?</button> <button id = "interes" class="boton" >¿Por interés?</button> <button id="verdad" class="boton">¿Para decirte la verdad?</button>';
 
       setTimeout(() => {
-         var boton = document.querySelectorAll('.botones .boton');
+         var boton = document.querySelectorAll('#parrafo2 .botones .boton');
 
          for (let i = 0; i < boton.length; i++) {
             boton[i].style.backgroundColor = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 0.5)`
@@ -107,11 +154,18 @@ function siguienteParrafo(e) {
    complice.style.display = "none";
    observador.style.display = "none";
    elegido.style.display = "none";*/
+
    parrafo2 = new Typed('.texto2', {
       strings: [contenido],
       typeSpeed: 35
    })
+
+   venganza = document.getElementById('venganza');
+   interes = document.getElementById('interes');
+   verdad = document.getElementById('verdad');
 }
+
+
 
 
 
