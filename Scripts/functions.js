5cfbe22d7b39fda450ef2a8ac0103e444cd8d6d6
Diego Go <sajudian85@gmail.com>
2023-06-30 22:08:37 -0500

    eliminando una imagen

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -62,7 +62,7 @@ function cambiarFondo() {
 
 //PONER EVIDENCIAS
 function ponerEvi() {
-   let maximoEV = 83;
+   let maximoEV = 82;
    var evidencia = Math.floor(Math.random() * maximoEV);
    document.getElementById('fondo').style.background = `url('../src/IMGS/EVDS/${evidencia}.jpg') center center / cover`;
 }
