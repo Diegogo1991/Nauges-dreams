1c0b78785947cb0ed2e2c9d86591a5cb955092e9
Diego Go <sajudian85@gmail.com>
2023-07-02 11:56:42 -0500

    Agregando 2 carta Supra

--- Scripts/Supra/jsSupra.js
+++ Scripts/Supra/jsSupra.js
@@ -4,10 +4,18 @@ import { imprimirTexto } from "../jsFuncTexto.js";
 
 import { textos } from "./textosSupra.js";
 
+//CARTA 1
 document.getElementById('1').addEventListener('click', () => {
    imprimirTexto(textos.carta1);
 });
 
+//CARTA 2
+document.getElementById('2').addEventListener('click', () => {
+   imprimirTexto(textos.carta2);
+});
+
+
+//FUNCION PARA RETROCEDER
 document.getElementById('atras').addEventListener('click', () => {
    window.location.replace('../coleccion.html');
 })
