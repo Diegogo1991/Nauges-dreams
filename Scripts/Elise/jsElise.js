842c6044e45afb613ea5db92bc23bb8cdfd70451
Diego Go <sajudian85@gmail.com>
2023-07-04 21:41:56 -0500

    Empezando con Elise

---
+++ Scripts/Elise/jsElise.js
@@ -0,0 +1,28 @@
+//Importación función y textos
+
+import { imprimirTexto } from "../jsFuncTexto.js";
+
+import { textos } from "./textosElise.js";
+
+//CARTA 1
+/*document.getElementById('1').addEventListener('click', () => {
+   imprimirTexto(textos.carta1);
+});*/
+
+//CARTA 2
+/*document.getElementById('2').addEventListener('click', () => {
+   imprimirTexto(textos.carta2);
+});
+*/
+
+//CARTA 3
+document.getElementById('3').addEventListener('click', () => {
+   imprimirTexto(textos.carta3);
+});
+
+
+
+//FUNCION PARA RETROCEDER
+document.getElementById('atras').addEventListener('click', () => {
+   window.location.replace('../coleccion.html');
+})
---
+++ Scripts/Elise/textosElise.js
@@ -0,0 +1,13 @@
+//TEXTOS para la pagina Supra
+
+const carta1 = '';
+
+const carta2 = '';
+
+const carta3 = 'Disculpa mi horrible crimen de compararte con la vida.<br><br><br>Amor de mi vida.<br><br><br>Responsabilidad gigante.<br><br><br>Sin embargo, de ambas he aprendido y de la mejor manera.<br><br><br>Que hay que darles duro, pero con amor.<br><br><br>Amor de mi vida.<br><br><br>Vida de amor. <br><br><br>No me gusta la poesía, esto solo fue una burla y una confesión.';
+
+export const textos = {
+   carta1,
+   carta2,
+   carta3
+}
