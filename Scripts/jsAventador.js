cb957270982526fddf5713ec1a472f6f24593273
Diego Go <133179164+Diegogo1991@users.noreply.github.com>
2023-07-05 23:49:42 -0500

    Eliminando script Aventador

--- Scripts/jsAventador.js
+++
@@ -1,7 +0,0 @@
-import { imprimirTexto } from "./jsFuncTexto.js";
-
-import { chatConElla } from "./textosAventador.js";
-
-document.getElementById('prueba').addEventListener('click', () => {
-   imprimirTexto(chatConElla);
-});
