e5a71bcd638b4249db115504512abf88f323cd35
Diego Go <133179164+Diegogo1991@users.noreply.github.com>
2023-07-07 17:39:23 -0500

    Probando imprimir textos desde word

--- Scripts/Aventador/jsAventador.js
+++ Scripts/Aventador/jsAventador.js
@@ -41,17 +41,7 @@ function imprimirTexto() {
    body.appendChild(divTablero);
 
    //llamar funcion para poner texto
-   var fileUrl = 'docanaa.txt';
-   var xhr = new XMLHttpRequest();
-   xhr.open('GET', fileUrl, true);
-   xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-         var contenido = xhr.responseText;
-         var textContentDiv = document.getElementById('text-content');
-         textContentDiv.innerText = contenido;
-      }
-   };
-   xhr.send();
+   insertarWord();
  
    let back = document.createElement('div');
    back.id = 'back';
@@ -64,4 +54,78 @@ function imprimirTexto() {
          atras.style.display = "block";
    })
    
+}
+
+//PARA IMPORTAR TEXTO DESDE WORD
+// Obtener el archivo de Word mediante fetch()
+function insertarWord() { 
+   var fileUrl = '../Scripts/Aventador/docana.docx';
+
+   fetch(fileUrl)
+      .then(function (response) {
+         if (!response.ok) {
+            throw new Error('Error al cargar el archivo.');
+         }
+         return response.arrayBuffer();
+      })
+      .then(function (arrayBuffer) {
+         // Convertir el documento de Word en HTML
+         var options = {
+            arrayBuffer: arrayBuffer
+         };
+
+         mammoth.extractRawText(options)
+            /*.then(function (result) {
+               var htmlContent = result.value;
+
+               // Mostrar el contenido en el elemento div
+               var wordContentDiv = document.getElementById('text-content');
+               wordContentDiv.innerHTML = htmlContent;
+            })
+            .done();*/
+            .then(function (result) {
+               var rawText = result.value;
+
+               // Procesar el texto para aplicar estilos HTML
+               var processedText = procesarTexto(rawText);
+
+               // Mostrar el contenido en el elemento div
+               var wordContentDiv = document.getElementById('text-content');
+               wordContentDiv.innerHTML = processedText;
+            })
+            .catch(function (error) {
+               console.error('Error al convertir el documento de Word:', error);
+            });
+      })
+      .catch(function (error) {
+         console.error(error);
+      });
+}
+
+function procesarTexto(rawText) {
+   // Reemplazar saltos de línea con etiquetas <br>
+   var processedText = rawText.replace(/\n/g, '<br>');
+
+   // Aplicar estilos al texto según ciertas reglas
+   // Ejemplo: si el texto contiene "Rojo:", aplicar color rojo al siguiente contenido
+   processedText = processedText.replace(/Rojo:(.*?)<br>/g, '<span style="color: red;">$1</span><br>');
+
+   // Puedes agregar más reglas aquí según tus necesidades
+
+   return processedText;
+}
+ 
+//PARA IMPORTAR TEXTO DESDE ARCHIVO .TXT
+function insertarText() {
+   var fileUrl = '../Scripts/Aventador/docana.txt';
+   var xhr = new XMLHttpRequest();
+   xhr.open('GET', fileUrl, true);
+   xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+         var contenido = xhr.responseText;
+         var textContentDiv = document.getElementById('text-content');
+         textContentDiv.innerText = contenido;
+      }
+   };
+   xhr.send();
 }
